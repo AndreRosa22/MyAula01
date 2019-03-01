11797684cc5fd37b3541a7c2583989055eb7397f
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-01 11:27:06 +0000

    Changed TiposVarios and Argumentos

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -6,7 +6,10 @@ namespace Argumentos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args);
+            for (int n = 0; n < args.Length; n++)
+            {
+                Console.WriteLine(args[n]);
+            }
             Console.ReadKey();
         }
     }
--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -11,16 +11,35 @@ namespace TiposVarios
             int b = 54;
             char c = '\u00A9';
             char d = '\u0026';
+            double e = 3;
+            double f = -7;
+            bool g = true;
+            bool h = false;
+
 
             //Imprime o valor da variavel a
+            Console.WriteLine("2 inteiros");
             Console.WriteLine(a);
             Console.WriteLine("");
             Console.WriteLine(b);
             Console.WriteLine("");
+            Console.WriteLine("2 carateres");
             Console.WriteLine(c);
             Console.WriteLine("");
             Console.WriteLine(d);
             Console.WriteLine("");
+            Console.WriteLine("2 reais");
+            Console.WriteLine(e);
+            Console.WriteLine("");
+            Console.WriteLine(f);
+            Console.WriteLine("");
+            Console.WriteLine("2 booleanos");
+            Console.WriteLine(g);
+            Console.WriteLine("");
+            Console.WriteLine(h);
+            Console.WriteLine("");
+            Console.WriteLine();
+
             Console.WriteLine("Press Enter to close");
             Console.ReadKey();
         }
