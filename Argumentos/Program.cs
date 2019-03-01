b94a8c6572f0f5d4dda0fb2a2704343058ec1f16
AndreRosa22 <andrefrancorosa@gmail.com>
2019-03-01 10:13:03 +0000

    Added Argumentos

---
+++ Argumentos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args);
+            Console.ReadKey();
+        }
+    }
+}
